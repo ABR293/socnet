62300e7e08f36d8326a8f86214ab70d78a410a41
ABR293 <Varyag-93@mail.ru>
2019-10-03 12:14:07 +0300

    Version 03.10.2019 12:13 - стабильная версия перед рефакторингом state / store.

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -2,8 +2,84 @@ let rerenderEntireTree = () =>{
     console.log('Change');
 };
 
+let store = {
 
+    _state: {
+        dialogs: [
+            {
+                id: 12345, name: 'dimon', messages: [
+                    {avatar: 1, text: 'adsbnvnvnvad', activity: true},
+                    {avatar: 1, text: 'dfhhgrdgrdgd1', activity: true},
+                    {avatar: 1, text: 'dasdg cvxfgg1', activity: true},
+                    {avatar: 1, text: 'fgcfdfdfsdffg', activity: true},
+                ]
+            },
+            {
+                id: 98615, name: 'peter', messages: [
+                    {avatar: 1, text: 'ghgfhgfdsfsdfdsfdsfdsfad', activity: true},
+                    {avatar: 1, text: 'dfhgfghdfsfsdafd1', activity: true},
+                    {avatar: 1, text: 'dasdtytry1', activity: true},
+                    {avatar: 1, text: 'fgttyfopoipipg', activity: true},
+                ]
+            },
+            {
+                id: 16576, name: 'andrey', messages: [
+                    {avatar: 1, text: 'ghgfhgfad', activity: true},
+                    {avatar: 1, text: 'dfhgfghd1', activity: true},
+                    {avatar: 1, text: 'dasdtytry1', activity: true},
+                    {avatar: 1, text: 'fgttyfopoipipg', activity: true},
+                ]
+            },
+            {
+                id: 78678, name: 'denis', messages: [
+                    {avatar: 1, text: 'adsaytyrtyd', activity: true},
+                    {avatar: 1, text: 'dfhyrtyrhd1', activity: true},
+                    {avatar: 1, text: 'dasdtedsdasdsartre1', activity: true},
+                    {avatar: 1, text: 'fgfrtertttg', activity: true},
+                ]
+            },
+            {
+                id: 64213, name: 'Misha', messages: [
+                    {avatar: 1, text: 'adttyrytryysad', activity: true},
+                    {avatar: 1, text: 'dfhftgdfghd1', activity: true},
+                    {avatar: 1, text: 'dasdtyruucsd1', activity: true},
+                    {avatar: 1, text: 'fg4yuyuyfcxzczxczxczxczxcg', activity: true},
+                ]
+            }
 
+        ],
+        profile: [
+
+            {
+                id: 8327178, name: 'ADMIN!!', avatar: '3', PostText: '', posts: [
+                    {id: 34522, name: 'sdsadas', message: "wfghcsfbzvddafvldmkvale;dkgjeg"},
+                    {id: 37867, name: 'rewe', message: "wthtbvsdrtarsteale;dkgjeg"},
+                    {id: 45452, name: 'sdsadas', message: "wfksadaifgyit7iyujvbcbrqergsdfkjsaldmkvale;dkgjeg"},
+
+                ]
+            },
+        ],
+    },
+
+    addPost(message) {
+        let NewPost = {
+            id: 89080,
+            name: 'Vaso',
+            message: message,
+        };
+    },
+    changePostText (text) {
+        state.profile[0].PostText = text;
+        rerenderEntireTree();
+    },
+
+    subscribe (observer) {
+        rerenderEntireTree = observer;
+    }
+
+};
+
+window.store = store;
 
 let state = {
    dialogs: [
@@ -52,7 +128,6 @@ let state = {
 
 };
 
-//rerenderEntireTree();
 
  export const addPost = (message) =>{
 
