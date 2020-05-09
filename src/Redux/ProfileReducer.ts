6a03b71c2abc908fdd952d1f14444287d88ca163
ABR293 <ABR293@yandex.ru>
2020-05-09 20:42:15 +0300

    затипизировал AuthAPI,

--- src/Redux/ProfileReducer.ts
+++ src/Redux/ProfileReducer.ts
@@ -1,24 +1,55 @@
 import {ProfileAPI} from "../API/API";
 import {stopSubmit} from "redux-form";
+import { ChangePageActionType } from "./UsersReducer";
+import { AppStateType } from "./Store";
+import { Dispatch } from "redux";
+import { ThunkAction } from "redux-thunk";
 //import {getState} from "redux-thunk";
 const ADD_POST = "socNet/profile/ADD-POST";
 const CHANGE_POST_TEXT = "socNet/profile/CHANGE-POST-TEXT";
 const SET_USER_PROFILE = "socNet/profile/SET-USER-PROFILE";
-const TOGGLE_IS_FETCHING = "socNet/profile/TOGGLE-IS-FETCHING";
+const TOGGLE_IS_FETCHING = "socNet/profile/TOGGLE-IS-FE TCHING";
 const SET_STATUS = "socNet/profile/SET-STATUS";
-const SAVE_PHOTO_SUCCESS = "SAVE-PHOTO-SUCCESS";
+const SAVE_PHOTO_SUCCESS = "socNet/profile/SAVE-PHOTO-SUCCESS";
 
-export const setStatus = (status: string ) => ({type: SET_STATUS, status: status});
-export const addPost = (message: string) => ({type: ADD_POST, message: message});
-export const changeText = (text: string) => ({type: CHANGE_POST_TEXT, text: text});
-export const setUserProfile = (data: any) => ({type: SET_USER_PROFILE, data});
-export const toggleFetching = (isFetching: boolean) => ({type: TOGGLE_IS_FETCHING, isFetching});
-export const savePhotoSuccess = (photo: any) => ({type:SAVE_PHOTO_SUCCESS , photo: photo});
+type SetStatusActionType = {
+    type: typeof SET_STATUS
+    status: string | null
+}
+type AddPostActionType = {
+    type: typeof ADD_POST
+    message: string
+}
+type ChangeTextActionType = {
+    type: typeof CHANGE_POST_TEXT
+    text: string
+}
+type SetUserProfileActionType = {
+    type: typeof SET_USER_PROFILE
+    data: any
+}
+type ToggleFetchingActionType = {
+    type: typeof TOGGLE_IS_FETCHING
+    isFetching: boolean
+}
+type SavePhotoSuccessActionType = {
+    type: typeof SAVE_PHOTO_SUCCESS
+    photo: any
+}
+export const setStatus = (status: string | null): SetStatusActionType => ({type: SET_STATUS, status: status});
+export const addPost = (message: string): AddPostActionType => ({type: ADD_POST, message: message});
+export const changeText = (text: string): ChangeTextActionType => ({type: CHANGE_POST_TEXT, text: text});
+export const setUserProfile = (data: any): SetUserProfileActionType => ({type: SET_USER_PROFILE, data});
+export const toggleFetching = (isFetching: boolean): ToggleFetchingActionType => ({type: TOGGLE_IS_FETCHING, isFetching});
+export const savePhotoSuccess = (photo: any): SavePhotoSuccessActionType => ({type: SAVE_PHOTO_SUCCESS, photo: photo});
 
+type ActionTypes = SetUserProfileActionType | AddPostActionType | ChangePageActionType | SetStatusActionType |
+SetUserProfileActionType | ToggleFetchingActionType | SetUserProfileActionType | SavePhotoSuccessActionType 
+ 
 let initialState = {
-    id: 4986 as number | null,
+    id: 4986 as number,
     fullName: "" as string,
-    status: '' as string,
+    status: '' as string | null,
     isFetching: false as boolean,
     userId: 4986 as number | null,
     avatar: '' as string,
@@ -45,9 +76,15 @@ let initialState = {
 
 export type initialStateType = typeof initialState
 
-export const profileReducer = (state = initialState, action: any) => {
+export const profileReducer = (state = initialState, action: ActionTypes ) => {
 
     switch (action.type) {
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            }
+        }
         case ADD_POST: {
             return {
                 ...state,
@@ -71,13 +108,7 @@ export const profileReducer = (state = initialState, action: any) => {
         case TOGGLE_IS_FETCHING: {
             return {
                 ...state,
-                isFetching: action.isFetching
-            }
-        }
-        case SET_STATUS: {
-            return {
-                ...state,
-                status: action.status
+                isFetching: action. isFetching
             }
         }
         case SAVE_PHOTO_SUCCESS: {
@@ -89,33 +120,40 @@ export const profileReducer = (state = initialState, action: any) => {
         default: {
             return state
         }
-
     }
 };
 
-export const setProfile = (userId:number ) => async(dispatch: any) => {
+
+type getStateType = () => AppStateType
+type DispatchType = Dispatch<ActionTypes>
+type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionTypes>
+
+export const setProfile = (userId:number | null):ThunkType=> async(dispatch:DispatchType) => {
     dispatch(toggleFetching(true));
     let response = await ProfileAPI.setProfile(userId);
         //console.log(response);
         dispatch(setUserProfile(response));
         dispatch(toggleFetching(false));
 };
-export const setUserStatus = (id: number) => async (dispatch:any) => {
+export const setUserStatus = (id: number):ThunkType => async (dispatch:DispatchType) => {
     let response = await ProfileAPI.getStatus(id);
     dispatch(setStatus(response))
 };
 
 
-export const updateUserStatus = (status: string) => async (dispatch:any) => {
+export const updateUserStatus = (status: string):ThunkType => async (dispatch:DispatchType) => {
     let response = await ProfileAPI.updateStatus(status);
     !response.data.resultCode && dispatch(setStatus(status))
 };
-export const savePhoto = (photo: any) => async(dispatch:any) => {
+export const savePhoto = (photo: any):ThunkType => async(dispatch:DispatchType) => {
     let response = await ProfileAPI.savePhoto(photo);
     console.log(response);
     !response.data.resultCode && dispatch(savePhotoSuccess(response.data.data.photos))
 };
-export const saveProfile = (profile:any ) => async(dispatch: any , getState: any) => {
+
+// It`s need to get more information, about types and how to to make types of this thunk
+
+export const saveProfile = (profile:any ):ThunkType => async(dispatch:any , getState: () => AppStateType) => {
     const userId = getState().auth.userId;
     let response = await ProfileAPI.saveProfile(profile);
     console.log(response);
